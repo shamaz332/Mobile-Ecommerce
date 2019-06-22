9fec6d56ed815f902e67041ab3e9ea1ca2dfe6f4
shamaz323 <shamazsaeed323@gmail.com>
2019-06-22 08:29:28 -0700

    footer change

--- client/src/components/pages/footer.js
+++ client/src/components/pages/footer.js
@@ -9,16 +9,16 @@ export default class Footer extends Component {
             <div class="row">
               <div class="col l6 s12">
                 <h5 class="white-text">We Provide</h5>
-                <p class="grey-text text-lighten-4">You can use rows and columns here to organize your footer content.</p>
+                <p class="grey-text text-lighten-4">Quality products with 6 days Money back gaurantee</p>
               </div>
               <div class="col l4 offset-l2 s12">
-                <h5 class="white-text">Links</h5>
-                <ul>
+                {/* <h5 class="white-text">Links</h5> */}
+                {/* <ul>
                   <li><a class="grey-text text-lighten-3" href="#!">Link 1</a></li>
                   <li><a class="grey-text text-lighten-3" href="#!">Link 2</a></li>
                   <li><a class="grey-text text-lighten-3" href="#!">Link 3</a></li>
                   <li><a class="grey-text text-lighten-3" href="#!">Link 4</a></li>
-                </ul>
+                </ul> */}
               </div>
             </div>
           </div>
