4946208b52d63301260068f00cd16fcc16231fad
shamaz323 <shamazsaeed323@gmail.com>
2019-07-23 01:21:14 -0700

    login Resolvedd

--- validation/login.js
+++ validation/login.js
@@ -7,16 +7,16 @@ module.exports = function validateLoginInput(data) {
   data.password = !isEmpty(data.password) ? data.password : "";
 // Email checks
   if (Validator.isEmpty(data.email)) {
-    errors.email = alert('Email field is required');
+    errors.email = "Email field is required";
   } else if (!Validator.isEmail(data.email)) {
-    errors.email = alert('Email is invalid');
+    errors.email = "Email is invalid";
   }
 // Password checks
   if (Validator.isEmpty(data.password)) {
-    errors.password = alert('Password field is required');
+    errors.password = "Password field is required";
   }
 return {
     errors,
-    isValid: isEmpty(alert(errors))
+    isValid: isEmpty(errors)
   };
 };
