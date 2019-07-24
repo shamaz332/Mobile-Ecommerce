8d335329d4f6ecead271c9bace0e554a30036fbe
shamaz323 <shamazsaeed323@gmail.com>
2019-07-24 13:17:53 -0700

    6th about database

--- client/src/redux/action/mobilesAction.js
+++ client/src/redux/action/mobilesAction.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export function getMobiles(){
   return function(dispatch){
-    axios.get("http://localhost:3001/mobiles")
+    axios.get("/mobiles")
       .then(function(response){
         dispatch({type:"GET_MOBILES", payload:response.data})
       })
